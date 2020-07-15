c30a76c3a173b95fb30114b57231a331d7a65b42
MrWilllian <willian.dev10@gmail.com>
2020-07-15 16:44:39 -0300

    feat: include Wrapper and GlobalStyles - v1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import Wrapper from './components/Wrapper';
+import GlobalStyles from './styles/GlobalStyles';
+
 function App() {
   return (
-    <div>
-    </div>
+    <>
+      <Wrapper />
+      <GlobalStyles />
+    </>
   );
 }
 
