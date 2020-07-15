623c08afcc1832b9c0fa0961971e980cdb8d529a
MrWilllian <willian.dev10@gmail.com>
2020-07-15 16:41:47 -0300

    style: change to recursive

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -23,6 +23,14 @@ export default createGlobalStyle`
     background-size: cover;
 
     font-family: 'Raleway', sans-serif;
+
+    @media(min-width: 800px) {
+      background-color: var(--very-dark-blue);
+      background-image: url('images/bg-desktop.png');
+      background-attachment: fixed;
+      background-position: center bottom;
+      background-size: auto;
+    }
   }
 
   :root {
@@ -30,7 +38,7 @@ export default createGlobalStyle`
     --grayish-blue: hsl(229, 7%, 55%);
     --dark-blue: hsl(228, 56%, 26%);
     --very-dark-blue: hsl(229, 57%, 11%);
-    --gradient: linear-gradient(hsl(6, 100%, 80%), hsl(335, 100%, 65%));
+    --gradient: linear-gradient(to right, hsl(6, 100%, 80%), hsl(335, 100%, 65%));
 
     --white: #FFF;
 
